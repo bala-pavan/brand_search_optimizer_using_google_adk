e9c5180ec845d2049d797695861c556d3add9a70
Adabala Bala Pavan Kumar <adabala_balapavankumar@epam.com>
2026-03-11 17:52:19 +0530

    add remaining source files

---
+++ sub_agents/comparison/agent.py
@@ -0,0 +1,26 @@
+from google.adk.agents.llm_agent import Agent
+
+from ...shared_libraries import constants
+from . import prompt
+
+comparison_generator_agent = Agent(
+    model=constants.MODEL,
+    name="comparison_generator_agent",
+    description="A helpful agent to generate comparison.",
+    instruction=prompt.COMPARISON_AGENT_PROMPT,
+)
+
+comparsion_critic_agent = Agent(
+    model=constants.MODEL,
+    name="comparison_critic_agent",
+    description="A helpful agent to critique comparison.",
+    instruction=prompt.COMPARISON_CRITIC_AGENT_PROMPT,
+)
+
+comparison_root_agent = Agent(
+    model=constants.MODEL,
+    name="comparison_root_agent",
+    description="A helpful agent to compare titles",
+    instruction=prompt.COMPARISON_ROOT_AGENT_PROMPT,
+    sub_agents=[comparison_generator_agent, comparsion_critic_agent],
+)
